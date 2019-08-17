5a404aeebb27e8632a859ea1e2d813a27fb4bdd0
John Ewart <john@johnewart.net>
2019-08-17 13:54:18 -0700

    Fixes

--- yourbased.sh
+++ yourbased.sh
@@ -11,7 +11,7 @@ export RAILS_ENV=test
 export RACK_ENV=test
 export MERB_ENV=test
 export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
-apt-get update && apt-get install -y tzdata postgresql-client
+apt-get update && apt-get install -y tzdata postgresql-client libpq-dev libcurl4-openssl-dev cmake
 gem install bundler -v 2.0.1
 
 export TRAVIS_BUILD_DIR=$PWD
@@ -21,6 +21,9 @@ source tools/ci/before_install.sh
 
 #install
 bundle install --jobs=3 --retry=3
+
+git config --global user.email "user@domain.com"
+git config --global user.name "git user"
 
 #before_script
 bundle exec rake test:$TEST_SUITE:setup
