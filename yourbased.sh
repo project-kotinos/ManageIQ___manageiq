f3869ca07a61363adff9b8356dc3604c259e4451
John Ewart <john@johnewart.net>
2019-08-17 14:16:21 -0700

    Add libxml2

--- yourbased.sh
+++ yourbased.sh
@@ -11,7 +11,7 @@ export RAILS_ENV=test
 export RACK_ENV=test
 export MERB_ENV=test
 export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
-apt-get update && apt-get install -y tzdata postgresql-client libpq-dev libcurl4-openssl-dev cmake
+apt-get update && apt-get install -y tzdata postgresql-client libpq-dev libcurl4-openssl-dev cmake libxml2-dev
 gem install bundler -v 2.0.1
 
 export TRAVIS_BUILD_DIR=$PWD
